bbe67a24da37c7a12224d7387ced1764fcbadb61
Felix posey <34116619+whitetrashyt@users.noreply.github.com>
2020-08-02 08:52:13 -0400

    Fix PID issues, add embeds.
    = Fixed issue regarding PID overflow
    = Decluttering
    + Added embeds for non webm tags
    
    Thank you to Marloes#1337 on discord for helping with this build!

--- main.py
+++ main.py
@@ -12,53 +12,59 @@ from discord.ext import commands as commands
 # functions from our r34botdl script on the github
 # the xml parse func
 def xmlparse(str):
-    xmlurl = u.urlopen(str)
-    # opens the url/xml file 
-    tree = et.parse(xmlurl)
-    # sets our xml tree to the parsed xml file
-    root = tree.getroot()
-    for i in root.iter('post'):
-        # for every root iteration in the sub-tree "post,"
-        fileurl = i.attrib['file_url']
-        return fileurl
+	xmlurl = u.urlopen(str)
+	# opens the url/xml file 
+	tree = et.parse(xmlurl)
+	# sets our xml tree to the parsed xml file
+	root = tree.getroot()
+	for i in root.iter('post'):
+		# for every root iteration in the sub-tree "post,"
+		fileurl = i.attrib['file_url']
+		return fileurl
 
 # asynch calls needs us to sync r34 each time
 r = rule34.Sync()
 # we define the xml grab and parse now as a func
 def r34dl(tags,int):
-    print(int)
-    xurl = r.URLGen(tags=tags, limit=1,PID=int) # this causes a problem if the pages for a specific tab are not above or equal to the supplied integer. Too bad!
-    wr = xmlparse(xurl)
-    return wr
+	print(f"int: {int}") # always use f-string :3
+	xurl = r.URLGen(tags=tags, limit=1,PID=int) # this causes a problem if the pages for a specific tab are not above or equal to the supplied integer. Too bad!
+	wr = xmlparse(xurl)
+	return wr
 
 
 
 def rprefix(str, r): 
-    rstr = str.replace(r,"")
-    return rstr
+	rstr = str.replace(r,"")
+	return rstr
 
-bot = commands.Bot(command_prefix='$')
-client = discord.Client() # define our client
+client = commands.Bot(command_prefix='$') # you did had bot = commands but should actually be client ^-^ / oh my god thank you
+Client = discord.Client() # define our client
 
 @client.event # an event to print on ready
 async def on_ready():
-    print("Logged in as {0.user}".format(client))
-    
+	print(f'Logged in as {client.user.name}!') # always use f-string :3
+	
 @client.event # event that listens for message that contains our prefix and a command
 async def on_message(message):
-    if message.author == client.user:
-        return
-    # prints the message into our console for debug
-    print(message.content)
+	if message.author == client.user:
+		return
+	# prints the message into our console for debug
+	print(f"Message Content: {message.content}") # always use f-string :3
 
-    if message.content.startswith('$porn'): # since discord.ext isn't working, we'll have to use a very unorthodox method
-        tags = rprefix(message.content, '$porn')
-        answer = r34dl(tags,random.randint(1,100))
-        await message.channel.send(answer + 'made by jess#0101')
-
-
-
+	if message.content.startswith('$porn'): # since discord.ext isn't working, we'll have to use a very unorthodox method
+		tags = rprefix(message.content, '$porn')
+		answer = r34dl(tags,random.randint(1,100))
+		if answer is None:
+			answer = r34dl(tags,1)
+		embed = discord.Embed(title=f'{message.content}', color=message.author.color)
+		embed.set_author(name=f'{message.author.display_name}', icon_url=f'{message.author.avatar_url}')
+		embed.set_thumbnail(url='https://rule34.paheal.net/themes/rule34v2/rule34_logo_top.png')
+		embed.set_image(url=f'{answer}')
+		embed.set_footer(text="Made by jess#0101", icon_url='https://cdn.discordapp.com/avatars/268211297332625428/a_06db63e4477b4a327f8a689f559bf887.gif?size=128')
+		if 'webm' in answer:
+			await message.channel.send(answer)
+		else:
+			await message.channel.send(embed=embed)
 
 # logs in to the bot
 client.run('enter token here')
-
